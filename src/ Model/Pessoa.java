164a4be16366eb33b54c3ea0e294324a43b578a5
NicoliZdaSilva <nicolizdasilva@gmail.com>
2024-11-22 10:17:34 -0300

    primeiro commit

---
+++ src/ Model/Pessoa.java
@@ -0,0 +1,12 @@
+package Model
+
+public abstract class static Pessoa(){
+    String nome;
+    String idade;
+    String cpf;
+
+    public Pessoa(String nome, String idade, String cpf){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
